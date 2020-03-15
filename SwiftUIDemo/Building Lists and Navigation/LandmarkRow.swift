5101dae8587891c9c84b09c47447d4007eaf467f
twototwoto <2637405168@qq.com>
2020-03-15 20:00:27 +0800

    添加预览固定长度，多个显示使用Group封装

--- SwiftUIDemo/Building Lists and Navigation/LandmarkRow.swift
+++ SwiftUIDemo/Building Lists and Navigation/LandmarkRow.swift
@@ -13,13 +13,23 @@ struct LandmarkRow: View {
     
     var body: some View {
         HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
             Text(landmark.name)
+            
+            Spacer()
         }
     }
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkRow(landmark: landmarkData[0])
+        Group{
+        LandmarkRow(landmark: landmarkData[1])
+            .previewLayout(.fixed(width:300, height:70))
+        LandmarkRow(landmark: landmarkData[2])
+        .previewLayout(.fixed(width:300, height:70))
+        }
     }
 }
