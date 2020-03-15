7f7f6ba2f04c586332a907ee141a56bd0299ed89
twototwoto <2637405168@qq.com>
2020-03-15 18:12:44 +0800

    Hello world 项目初始化

---
+++ SwiftUIDemo/SwiftUIDemoView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIDemoView.swift
+//  SwiftUIDemo
+//
+//  Created by 王永旺永旺 on 2020/3/15.
+//  Copyright © 2020 WYW. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIDemoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIDemoView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIDemoView()
+    }
+}
